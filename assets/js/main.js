6c856f84714213424b0f8e6b56535001f06060ff
Peter Kazazes <peter@peterk.co>
2018-08-03 19:15:29 -0700

    Add youtube link type

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,22 @@
 jQuery(document).ready(function($) {
+  $(".level-bar-inner").css("width", "0");
 
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+  $(window).on("load", function() {
+    $(".level-bar-inner").each(function() {
+      var itemWidth = $(this).data("level");
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
+      $(this).animate(
+        {
+          width: itemWidth
+        },
+        800
+      );
     });
-   
-    
+  });
 
+  $(".youtube-modal").on("hide.bs.modal", function(e) {
+    var $this = $(this);
+    var $frame = $this.find("iframe");
+    $frame.attr("src", $frame.attr("src"));
+  });
 });
