46e71ca5648d126b93b13a6c54f1b4d972a87d14
Peter Kazazes <peter@peterk.co>
2018-08-03 19:49:26 -0700

    Convert to BS4 & styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,4 @@
 jQuery(document).ready(function($) {
-  $(".level-bar-inner").css("width", "0");
-
-  $(window).on("load", function() {
-    $(".level-bar-inner").each(function() {
-      var itemWidth = $(this).data("level");
-
-      $(this).animate(
-        {
-          width: itemWidth
-        },
-        800
-      );
-    });
-  });
-
   $(".youtube-modal").on("hide.bs.modal", function(e) {
     var $this = $(this);
     var $frame = $this.find("iframe");
