8328a32b752f627f1cd3e2fe429b3945cca3ba01
Peter Kazazes <peter@peterk.co>
2019-07-17 23:10:37 -0700

    update social links and byline

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
 jQuery(document).ready(function($) {
-  $(".youtube-modal").on("hide.bs.modal", function(e) {
-    var $this = $(this);
-    var $frame = $this.find("iframe");
-    $frame.attr("src", $frame.attr("src"));
-  });
+
 });
